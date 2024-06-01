f6d71c5c9bdb876958178d437a857c4cf2c51714
AresConnor <39639716+AresConnor@users.noreply.github.com>
2024-06-01 14:04:21 +0800

    Update profiler.py

--- profiler.py
+++ profiler.py
@@ -3,10 +3,13 @@ def installProfilerCallback():
     from typing import Any
     from types import FrameType
     def _(frame: FrameType, event: str, arg: Any):
-        code = frame.f_code
-        filename = code.co_filename
-        lineno = code.co_firstlineno
-        fn_name = code.co_name
-        print(f"{event:<10} | {filename}:{lineno} => {fn_name}({arg})")
+        try:
+            code = frame.f_code
+            filename = code.co_filename
+            lineno = code.co_firstlineno
+            fn_name = code.co_name
+            print(f"{event:<10} | {filename}:{lineno} => {fn_name}({arg})")
+        except:
+            pass
 
     sys.setprofile(_)
